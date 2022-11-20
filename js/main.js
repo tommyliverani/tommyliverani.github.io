14033313e67bdace0dd3b5b0a03f0985b3c548ba
Se3-UniBo <luca.ragazzi9696@gmail.com>
2022-11-20 11:50:18 +0100

    Test del cambio lingua

--- js/main.js
+++ js/main.js
@@ -114,4 +114,82 @@ jQuery(document).ready(function($) {
         });
 
 
+        ///// Language Switching (2 languages: English and Chinese). /////
+        
+        // Initially disable language switching button.
+        $('#switch-lang').css({'pointer-events':'none',
+            'cursor':'default'}).attr('disabled','disabled');
+        
+        function langButtonListen() {
+            $('#switch-lang').click(function (event) {
+                event.preventDefault();
+                $('[lang="zh"]').toggle();
+                $('[lang="en"]').toggle();
+                // Switch cookie stored language.
+                if ($.cookie('lang') === 'en') {
+                    $.cookie('lang', 'zh', { expires: 7 });
+                } else {
+                    $.cookie('lang', 'en', { expires: 7 });
+                }
+            });
+            // Enable lang switching button.
+            $('#switch-lang').css({'pointer-events':'auto', 'cursor':'pointer'}).removeAttr('disabled');
+        }
+        
+        // Check if language cookie already exists.
+        if ($.cookie('lang')) {
+            var lang = $.cookie('lang');
+            if (lang === 'it') {
+                $('[lang="en"]').hide();
+                langButtonListen();
+            } else {
+                $('[lang="it"]').hide();
+                langButtonListen();
+            }
+        } else {
+            // no cookie set, so detect language based on location.
+            if ("geolocation" in navigator) {
+                // geolocation is available
+                navigator.geolocation.getCurrentPosition(function (position) {
+                    // accepted geolocation so figure out which country
+                    var lat = position.coords.latitude, lng = position.coords.longitude;
+                    $.getJSON('http://maps.googleapis.com/maps/api/geocode/json?latlng='+lat+','+lng+'&sensor=true', null, function (response) {
+                        var country = response.results[response.results.length-1].formatted_address;
+                        if (country ===  'Italy') {
+                            $('[lang="en"]').hide();
+                            $.cookie('lang', 'it', { expires: 7 });
+                            langButtonListen();
+                        } else {
+                            $('[lang="it]').hide();
+                            $.cookie('lang', 'en', { expires: 7 });
+                            langButtonListen();
+                        }
+                    }).fail(function (err) {
+                        console.log('error: '+err);
+                        $('[lang="en"]').hide();
+                        $.cookie('lang', 'it', { expires: 7 });
+                        langButtonListen();
+                    });
+                },
+                function (error) {
+                    if (error.code == error.PERMISSION_DENIED) {
+                    // denied geolocation
+                        $('[lang="en"]').hide();
+                        $.cookie('lang', 'it', { expires: 7 });
+                        langButtonListen();
+                    } else {
+                        console.log('Unknown error. Defaulting to Italian!');
+                        $('[lang="en"]').hide();
+                        $.cookie('lang', 'it', { expires: 7 });
+                        langButtonListen();
+                    }
+                });
+            } else {
+                // geolocation IS NOT available
+                $('[lang="en"]').hide();
+                $.cookie('lang', 'it', { expires: 7 });
+                langButtonListen();
+            }
+        }
+
 });
